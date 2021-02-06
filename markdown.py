8235e1470595ab9ec4f85b9c12e2142b368eaa49
chris-greening <chrisgreening3@gmail.com>
2021-02-06 12:02:22 -0500

    add: created file for hardcoded markdown

---
+++ markdown.py
@@ -0,0 +1,7 @@
+INTRO = """
+Welcome to the interactive United States Energy Consumption dashboard! The purpose of this dashboard is to visualize and explore the US's total energy consumption from 1960 to 2018.
+
+_Notice of Non-Affiliation and Disclaimer_: We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with the United States government or any of its agencies/departments.
+
+Developed by [Chris Greening](https://www.linkedin.com/in/chris-greening-dev/)
+"""
