bbf82380f5ee957ccc16debae36d9e72d6ec85e4
chris-greening <chrisgreening3@gmail.com>
2021-02-11 15:38:55 -0500

    add: added test markdown for more space

--- markdown.py
+++ markdown.py
@@ -1,5 +1,5 @@
 INTRO = """
-Welcome to the interactive United States Energy Consumption dashboard! The purpose of this dashboard is to visualize and explore the US's total energy consumption from 1960 to 2018.
+Welcome to the interactive United States Energy Consumption dashboard! The purpose of this dashboard is to visualize and explore the US's total energy consumption from 1960 to 2018 from a variety of different perspectives. Welcome to the interactive United States Energy Consumption dashboard! The purpose of this dashboard is to visualize and explore the US's total energy consumption from 1960 to 2018 from a variety of different perspectives. Welcome to the interactive United States Energy Consumption dashboard! The purpose of this dashboard is to visualize and explore the US's total energy consumption from 1960 to 2018 from a variety of different perspectives.
 
 _Notice of Non-Affiliation and Disclaimer_: We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with the United States government or any of its agencies/departments.
 
