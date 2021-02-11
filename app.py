e7d9b8859b746c931985f7143f03a81e64c95ab8
chris-greening <chrisgreening3@gmail.com>
2021-02-11 15:38:39 -0500

    modify: reduced visual real estate down to one main plot

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ df = dp.load_dataset()
 primary_energy_df = dp.load_primary_energy_sources(df)
 
 # Precomputed figures
+us_main_plot_dict = pc.precompute_main_plots(df, primary_energy_df)
 us_primary_bar_dict = pc.us_primary_per_year(primary_energy_df)
 us_primary_pie_dict = pc.us_primary_per_year_pie(primary_energy_df)
 
@@ -36,20 +37,52 @@ app.layout = html.Div(children = [
     html.Div(
         children = [
             html.H1("United States Energy Consumption", style={"font-size": "6vh", "text-align": "center"}),
-            dcc.Markdown(markdown.INTRO),
+            dcc.Markdown(markdown.INTRO, id="intro-text"),
             html.Hr(),
             html.Div(
                 children = [
                     html.Div(
                         html.Div(
                             children=[
-                                html.H3(
-                                    "Total energy consumption",
-                                    className="plot-header"
+                                html.Div(
+                                    children = [
+                                        html.H3(
+                                            "Total energy consumption",
+                                            className="plot-header main-plot-header-col"
+                                        ),
+                                        dcc.RadioItems(
+                                            options=[
+                                                {'label': 'Total',
+                                                    'value': 'Total'},
+                                                {'label': 'Total (by resource)',
+                                                    'value': 'Total (by resource)'},
+                                                {'label': 'Resource',
+                                                    'value': 'Resource'},
+                                            ],
+                                            value='Total',
+                                            labelStyle={
+                                                'display': 'inline-block'},
+                                            className="main-plot-header-col",
+                                            id="main-plot-type"
+                                        ),
+                                        dcc.RadioItems(
+                                            options=[
+                                                {'label': 'Year',
+                                                    'value': 'Year'},
+                                                {'label': 'President',
+                                                    'value': 'President'},
+                                            ],
+                                            value='Year',
+                                            labelStyle={
+                                                'display': 'inline-block'},
+                                            className="main-plot-header-col",
+                                            id="x-axis-labels"
+                                        )
+                                    ],
+                                    className="row"
                                 ),
                                 dcc.Graph(
                                     id="us-total",
-                                    figure=pc.us_total(df, primary_energy_df),
                                     hoverData={"points": [{"x": "2018-01-01"}]},
                                     style={"height": plotting.PLOT_HEIGHT}
                                 )
@@ -77,52 +110,51 @@ app.layout = html.Div(children = [
                 ],
                 className="row"
             ),
-            html.Div(
-                children = [
-                    html.Div(
-                        html.Div(
-                            children=[
-                                html.H3(
-                                    "Resource usage",
-                                    className="plot-header"
-                                ),
-                                dcc.Graph(
-                                    id="us-total-stacked-area",
-                                    figure=pc.us_total_stacked_area(
-                                        primary_energy_df),
-                                    style={"height": plotting.PLOT_HEIGHT},
-                                    hoverData={"points": [
-                                        {"x": 2018}]},
-                                )
-                            ],
-                            className="plot"
-                        ),
-                        className="col-xl-8 order-xl-12"
-                    ),
-                    html.Div(
-                        html.Div(
-                            children=[
-                                html.H3(
-                                    id="us-primary-pie-header",
-                                    className="plot-header"
-                                ),
-                                dcc.Graph(
-                                    id="us-primary-pie",
-                                    style={"height": plotting.PLOT_HEIGHT}
-                                )
-                            ],
-                            className="plot"
-                        ),
-                        className="col-xl-4 order-xl-1"
-                    ),
-                ],
-                className="row"
-            )
+            # html.Div(
+            #     children = [
+            #         html.Div(
+            #             html.Div(
+            #                 children=[
+            #                     html.H3(
+            #                         "Resource usage",
+            #                         className="plot-header"
+            #                     ),
+            #                     dcc.Graph(
+            #                         id="us-total-stacked-area",
+            #                         figure=pc.us_total_stacked_area(
+            #                             primary_energy_df),
+            #                         style={"height": plotting.PLOT_HEIGHT},
+            #                         hoverData={"points": [
+            #                             {"x": 2018}]},
+            #                     )
+            #                 ],
+            #                 className="plot"
+            #             ),
+            #             className="col-xl-8 order-xl-12"
+            #         ),
+            #         html.Div(
+            #             html.Div(
+            #                 children=[
+            #                     html.H3(
+            #                         id="us-primary-pie-header",
+            #                         className="plot-header"
+            #                     ),
+            #                     dcc.Graph(
+            #                         id="us-primary-pie",
+            #                         style={"height": plotting.PLOT_HEIGHT}
+            #                     )
+            #                 ],
+            #                 className="plot"
+            #             ),
+            #             className="col-xl-4 order-xl-1"
+            #         ),
+            #     ],
+            #     className="row"
+            # )
         ],
         className="container-fluid dash"
     )
 ])
-
 
 ########## HEADERS
 @app.callback(
@@ -141,6 +173,17 @@ def us_primary_pie_header(hoverData):
 def us_primary_bar_header(hoverData):
     year_value = int(hoverData['points'][0]['x'][:4])
     return f"Total energy consumption ({year_value})"
+
+########## MAIN PLOT
+@app.callback(
+    Output('us-total', 'figure'),
+    [Input("main-plot-type", "value"),
+    Input("x-axis-labels", "value")]
+)
+def update_main_plot(depiction_type, x_axis_type):
+    fig_key = depiction_type + x_axis_type
+    fig = us_main_plot_dict[fig_key]
+    return fig
 
 ########## BAR PLOT
 
