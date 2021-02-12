cb67e3a75e309fe79f280e43079c1660ff1ec58f
chris-greening <chrisgreening3@gmail.com>
2021-02-11 22:32:46 -0500

    add: added bar and pie precomputations

--- plot_computations.py
+++ plot_computations.py
@@ -8,7 +8,7 @@ import plotting
 import presidents
 
 def precompute_main_plots(total_df, primary_df):
-    depiction_types = ["Total", "Total (by resource)", "Resource"]
+    depiction_types = ["Energy consumption", "Energy consumption (per capita)", "Energy consumption (per resource)", "Resource consumption"]
     x_axis_types = ["Year", "President"]
 
     combinations = [[depiction, x_axis] for depiction in depiction_types for x_axis in x_axis_types]
@@ -34,11 +34,11 @@ def us_total(total_df, primary_df, depiction_type, x_axis_type):
                    per_cap_df["Million BTU"].max())**5
 
 
-    if depiction_type == "Total":
-        consumption_df = consumption_df.groupby(["Year"], as_index=False).sum()
-        min_x, max_x, min_y, max_y = calculate_bounds(consumption_df)
+    total_df = consumption_df.groupby(["Year"], as_index=False).sum()
+    min_x, max_x, min_y, max_y = calculate_bounds(total_df)
+    if depiction_type == "Energy consumption":
         fig = px.scatter(
-            consumption_df,
+            total_df,
             x="Year",
             y="Quadrillion BTU",
             size=marker_size,
@@ -50,11 +50,32 @@ def us_total(total_df, primary_df, depiction_type, x_axis_type):
             # height=500,
             size_max=12
         )
-    elif depiction_type == "Total (by resource)":
-        min_x, max_x, min_y, max_y = calculate_bounds(consumption_df)
-        consumption_df = consumption_df.groupby(["Year", "Source"], as_index=False).sum()
+    elif depiction_type == "Energy consumption (per capita)":
+        min_x = datetime.date(1960, 1, 1)
+        min_y = per_cap_df["Million BTU"].min()
+        min_y = min_y - min_y*.05
+
+        max_x = datetime.date(2018, 1, 1)
+        max_y = per_cap_df["Million BTU"].max()
+        max_y = max_y + max_y*.05
+
+        fig = px.scatter(
+            per_cap_df,
+            x="Year",
+            y="Million BTU",
+            hover_name="Year",
+            size=marker_size,
+            color=per_cap_df["Million BTU"],
+            color_continuous_scale=px.colors.diverging.RdYlGn[::-1],
+            range_x=[min_x, max_x],
+            range_y=[min_y, max_y],
+            size_max=12
+        )
+    elif depiction_type == "Energy consumption (per resource)":
+        total_resource_df = consumption_df.groupby(["Year", "Source"], as_index=False).sum()
+        min_y = 0
         fig = px.area(
-            consumption_df,
+            total_resource_df,
             x="Year",
             y="Quadrillion BTU",
             color="Source",
@@ -63,11 +84,11 @@ def us_total(total_df, primary_df, depiction_type, x_axis_type):
             range_y=[min_y, max_y]
         )
     else:
-        consumption_df = consumption_df.groupby(
+        resource_df = consumption_df.groupby(
             ["Year", "Source"], as_index=False).sum()
-        min_x, max_x, min_y, max_y = calculate_bounds(consumption_df)
+        min_x, max_x, min_y, max_y = calculate_bounds(resource_df)
         fig = px.line(
-            consumption_df,
+            resource_df,
             x="Year",
             y="Quadrillion BTU",
             color="Source",
@@ -156,7 +177,7 @@ def add_presidential_axes(fig) -> None:
         )
         ticktext.append(president.name)
         tickvals.append(president.start)
-    fig.update_xaxes(showgrid=False, ticktext=ticktext, tickvals=tickvals)
+    fig.update_xaxes(showgrid=False, ticktext=ticktext, tickvals=tickvals, title="President")
     fig.update_yaxes(showgrid=False)
 
 def us_primary_per_year(primary_df):
@@ -179,28 +200,101 @@ def us_primary_per_year(primary_df):
                 range_y=[min_y, max_y],
                 color_discrete_map=plotting.ENERGY_SOURCE_COLORS
         )
-        fig.update_layout(plotting.PLOT_COLORS)
+        fig.update_layout(plotting.PLOT_COLORS, showlegend=False)
         fig.update_xaxes(title_text="", categoryorder="total ascending")
         figs[year] = fig
     return figs
 
-def us_primary_per_year_pie(primary_df):
-    consumption_df = dp.data_subset(
-        primary_df, states=["United States"], sectors=["Total"])
-    consumption_df = consumption_df.groupby(
-        ["Year", "Sector", "Source"], as_index=False).sum()
+def precompute_state_per_year(total_df):
+
+    # Prepare the datasets
+    primary_df = dp.load_primary_energy_sources(total_df)
+    primary_df = dp.data_subset(primary_df, states=[state for state in total_df["State"].unique(
+    ) if state != "United States"], sectors=["Total"])
+    primary_df["BTU"] = primary_df["BTU"]/1_000_000
+    primary_df = primary_df.rename(columns={"BTU": "Quadrillion BTU"})
+
+    per_cap_df = dp.data_subset(total_df, states=[state for state in total_df["State"].unique(
+    ) if state != "United States"], sources=["Total"], sectors=["Total consumption per capita"])
+    per_cap_df = per_cap_df.rename(columns={"BTU": "Million BTU"})
+
+
+    max_y = primary_df.groupby(["State", "Year"]).sum()["Quadrillion BTU"].max()
+    per_cap_max_y = per_cap_df.groupby(["State", "Year"]).sum()["Million BTU"].max()
+    max_y = max_y + max_y*.05
+
+    # Create all possible combinations of plots
+    depiction_types = ["Energy consumption", "Energy consumption (per capita)"]
+    years = total_df["Year"].unique()
+    combinations = [[depiction, year]
+                    for depiction in depiction_types for year in years]
+
+    figs = {}
+    for depiction, year in combinations:
+        if depiction == "Energy consumption":
+            year_df = primary_df[primary_df["Year"] == year]
+            fig = state_bar_plot(year_df, max_y)
+        else:
+            year_df = per_cap_df[per_cap_df["Year"] == year]
+            fig = state_per_cap_bar_plot(year_df, per_cap_max_y)
+        figs[depiction + str(year)] = fig
+    return figs
+
+def state_bar_plot(primary_df, max_y):
+    # Prepare the dataset
+
+    min_y = 0
+
+    fig = px.bar(
+        primary_df,
+        x="State",
+        y="Quadrillion BTU",
+        color="Source",
+        range_y=[min_y, max_y],
+        color_discrete_map=plotting.ENERGY_SOURCE_COLORS
+    )
+    fig.update_xaxes(title_text="", categoryorder="total ascending", tickfont=dict(size=8), tickmode="linear")
+    fig.update_layout(plotting.PLOT_COLORS, showlegend=False)
+    return fig
+
+def state_per_cap_bar_plot(per_cap_df, max_y):
+    min_y = 0
+
+    fig = px.bar(
+        per_cap_df,
+        x="State",
+        y="Million BTU",
+        color="Million BTU",
+        color_continuous_scale=px.colors.diverging.RdYlGn[::-1],
+        range_y=[min_y, max_y],
+        range_color=[0, max_y]
+    )
+    fig.update_xaxes(title_text="", categoryorder="total ascending",
+                     tickfont=dict(size=8), tickmode="linear")
+    fig.update_layout(plotting.PLOT_COLORS, showlegend=False)
+    return fig
+
+def precompute_pie_plot_per_year(primary_df):
+    consumption_df = dp.data_subset(primary_df, states=["United States"], sectors=["Total"])
+    consumption_df = consumption_df.groupby(["Year", "Sector", "Source"], as_index=False).sum()
     consumption_df["BTU"] = consumption_df["BTU"]/1_000_000
     consumption_df = consumption_df.rename(columns={"BTU": "Quadrillion BTU"})
 
+    min_y = 0
+    max_y = consumption_df["Quadrillion BTU"].max()
+    max_y = max_y + max_y*.05
+
     figs = {}
     for year in consumption_df["Year"].unique():
-        fig = px.pie(
-            consumption_df[consumption_df["Year"] == year],
-            values="Quadrillion BTU",
-            names="Source",
-            color="Source",
-            color_discrete_map=plotting.ENERGY_SOURCE_COLORS
+        fig = px.bar(
+                consumption_df[consumption_df["Year"] == year],
+                x="Source",
+                y="Quadrillion BTU",
+                color="Source",
+                range_y=[min_y, max_y],
+                color_discrete_map=plotting.ENERGY_SOURCE_COLORS
         )
-        fig.update_layout(plotting.PLOT_COLORS)
+        fig.update_layout(plotting.PLOT_COLORS, showlegend=False)
+        fig.update_xaxes(title_text="", categoryorder="total ascending")
         figs[year] = fig
     return figs
