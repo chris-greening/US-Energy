fe89cf7cda3b449299a1daeb5e4a8767f69c43ff
chris-greening <chrisgreening3@gmail.com>
2021-03-02 14:48:16 -0500

    modify: moved text around and changed font-size of Source header

--- markdown.py
+++ markdown.py
@@ -1,16 +1,17 @@
 INTRO = """
 Welcome to the United States Energy Consumption dashboard!
-The purpose of this dashboard is to explore U.S. energy consumption data
+The purpose of this dashboard is to interactively explore U.S. energy consumption data
 provided by the [Energy Information Administration](https://www.eia.gov/) between 1960
 and 2018.
 
-### Sources
+###### Sources
 - [State Energy Data System (SEDS): 1960-2018 consumption](https://www.eia.gov/state/seds/sep_use/total/csv/use_all_btu.csv)
 - [United States geographic shapefile](https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_20m.zip)
+- [Source code](https://github.com/chris-greening/US-Energy)
 
 _Notice of Non-Affiliation and Disclaimer_: We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with the United States government or any of its agencies/departments.
 
 Developed by [Chris Greening](https://www.linkedin.com/in/chris-greening-dev/)
 
-[Source code](https://github.com/chris-greening/US-Energy)
+**Click a specific year on the plot below for a breakdown of that year's energy consumption.**
 """
